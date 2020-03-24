80dbd65d6d390296412dd6ba2ce4bfcffebe5bec
Marius Kasparavičius <mariuskasparavicius@inbox.lt>
2020-03-24 13:20:17 +0200

    Small fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
           <div class="col-lg-4 col-md-6 mb-4">
             <div class="card h-100">
               <a href="#">
-                <img class="card-img-top" src="{{ $product->photo ? $product->photo->url : 'http://placehold.it/700x400' }}" alt="">
+                <img class="card-img-top" src="{{ $product->photo ? $product->photo->thumbnail : 'http://placehold.it/700x400' }}" alt="">
               </a>
               <div class="card-body">
                 <h4 class="card-title">
